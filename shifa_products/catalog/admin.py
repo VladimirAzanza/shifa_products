edf3636279fb0ac4bfbb398beefbe1241b9257bb
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-20 21:12:10 +0300

    Add some features for admin zone

--- shifa_products/catalog/admin.py
+++ shifa_products/catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Location, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'description',
+        'is_available',
+        'price',
+        'category_id',
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'slug',
+    )
+    search_fields = (
+        'slug',
+        'id'
+    )
+    list_editable = (
+        'slug',
+    )
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+    )
+    search_fields = (
+        'name',
+        'id'
+    )
+    list_editable = (
+        'name',
+    )
+
+
+admin.site.empty_value_display = 'Campo sin rellenar'
--- shifa_products/users/admin.py
+++ shifa_products/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
+
+
+@admin.register(user)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+        'is_active',
+        'date_joined'
+    )

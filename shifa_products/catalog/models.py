4d2676f1f2ea50f1a460b7cda6f79c34066bffd5
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-20 12:02:06 +0300

    Add category to models

--- shifa_products/catalog/models.py
+++ shifa_products/catalog/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=False,
+        help_text=(
+            'Required identifier for category URL;'
+            'Symbols, dashes, or underscores are acceptable.'
+        )
+    )
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='products/images/')
-    # category
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    # is_available
     # location
+    # reviews

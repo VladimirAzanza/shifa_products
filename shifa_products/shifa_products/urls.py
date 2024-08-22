c572507ad9130273a5e1b547d00671f7158926a5
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-22 15:11:39 +0300

    Add signup button

--- shifa_products/shifa_products/urls.py
+++ shifa_products/shifa_products/urls.py
@@ -1,26 +1,22 @@
-"""
-URL configuration for shifa_products project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserForm
 
 auth_urls = [
     path('', include('django.contrib.auth.urls')),
+    path(
+        'registration/',
+        CreateView.as_view(
+            template_name='registration/signup.html',
+            form_class=UserForm,
+            success_url=reverse_lazy('pages:index'),
+        ),
+        name='registration'
+    )
 ]
 
 urlpatterns = [

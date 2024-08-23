32d3706995c510484a0fa9825217e6449e5fe7a8
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-23 12:18:47 +0300

    Add address user model

--- shifa_products/users/models.py
+++ shifa_products/users/models.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .constants import MAX_LENTGH_ADDRESS_CONSTANTS
+from catalog.models import Location
+
 
 class CustomUser(AbstractUser):
     pass
-    #location + direction
+
+
+class AddressUser(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='addresses'
+    )
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, related_name='addresses'
+    )
+    street = models.CharField(max_length=MAX_LENTGH_ADDRESS_CONSTANTS)
+    postal_code = models.SmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return (
+            f'{self.user.username} -'
+            f'{self.location}, {self.street}, {self.postal_code}'
+        )

54e97f1d202028b0046633cfa7206d5519e85679
Vladimir Azanza <vladimirazanza@gmail.com>
2024-11-19 15:00:48 +0300

    Fix product list view to get a queryset from the category slug

--- shifa_products/catalog/views.py
+++ shifa_products/catalog/views.py
@@ -29,6 +29,19 @@ class ProductListView(ListView):
     paginate_by = 10
     context_object_name = 'products'
 
+    def get_category(self):
+        return get_object_or_404(
+            Category, slug=self.kwargs.get('category_slug')
+        )
+
+    def get_queryset(self):
+        return Product.objects.filter(category=self.get_category())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.get_category()
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product

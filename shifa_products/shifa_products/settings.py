a55b95e9e2ba9f69f6ea1ec24f75d318aebf9959
Vladimir Azanza <vladimirazanza@gmail.com>
2024-11-19 15:57:35 +0300

    Add templates to reset password

--- shifa_products/shifa_products/settings.py
+++ shifa_products/shifa_products/settings.py
@@ -114,3 +114,7 @@ INTERNAL_IPS = [
 if DEBUG:
     INSTALLED_APPS += ['debug_toolbar']
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+]

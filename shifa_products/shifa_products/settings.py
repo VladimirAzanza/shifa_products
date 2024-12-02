1ff628f6d1f9a0309b3a6bda5ae261d06f2516fa
Vladimir Azanza <vladimirazanza@gmail.com>
2024-12-02 23:56:48 +0300

    Fix problems with custom.css related to footer

--- shifa_products/shifa_products/settings.py
+++ shifa_products/shifa_products/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

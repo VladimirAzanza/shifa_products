66198c2fbdf0fef6853e38f011b96f6f83874f1e
Vladimir Azanza <vladimirazanza@gmail.com>
2024-12-04 20:04:17 +0300

    Add chatbot with tawk.to

--- shifa_products/shifa_products/settings.py
+++ shifa_products/shifa_products/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tawkto',
     'catalog.apps.CatalogConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',

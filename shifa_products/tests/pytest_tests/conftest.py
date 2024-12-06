7e1716e6e92b773f6cdd9180b426b62f68b4cac5
Vladimir Azanza <vladimirazanza@gmail.com>
2024-12-06 18:35:11 +0300

    Add some route tests

--- shifa_products/tests/pytest_tests/conftest.py
+++ shifa_products/tests/pytest_tests/conftest.py
@@ -1,9 +1,40 @@
 from django.urls import reverse
 import pytest
 
+from catalog.models import Category
+
 
 @pytest.fixture
 def index_url():
     return reverse(
         'pages:index'
+    )
+
+
+@pytest.fixture
+def about_us_url():
+    return reverse(
+        'pages:about_us'
+    )
+
+
+@pytest.fixture
+def category_url():
+    return reverse(
+        'catalog:category_list'
+    )
+
+
+@pytest.fixture
+def create_category():
+    return Category.objects.create(
+        name='Category 1', slug='category-1'
+    )
+
+
+@pytest.fixture
+def category_product_list_url(create_category):
+    return reverse(
+        'catalog:category_product_list',
+        args=(create_category.slug,)
     )
--- shifa_products/tests/pytest_tests/test_routes.py
+++ shifa_products/tests/pytest_tests/test_routes.py
@@ -9,6 +9,9 @@ from pytest_lazyfixture import lazy_fixture
     'name',
     (
         lazy_fixture('index_url'),
+        lazy_fixture('about_us_url'),
+        lazy_fixture('category_url'),
+        lazy_fixture('category_product_list_url')
     )
 )
 def test_get_routes_availability_for_anonymous_user(client, name):

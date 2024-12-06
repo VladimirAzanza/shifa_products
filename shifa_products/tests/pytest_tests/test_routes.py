1dd384b3d2c7d396c0cb8dd729ca805d5a81f1cb
Vladimir Azanza <vladimirazanza@gmail.com>
2024-12-06 21:46:46 +0300

    Test routes for auth client

--- shifa_products/tests/pytest_tests/test_routes.py
+++ shifa_products/tests/pytest_tests/test_routes.py
@@ -18,3 +18,19 @@ from pytest_lazyfixture import lazy_fixture
 def test_get_routes_availability_for_anonymous_user(client, name):
     response = client.get(name)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    (
+        lazy_fixture('index_url'),
+        lazy_fixture('about_us_url'),
+        lazy_fixture('category_url'),
+        lazy_fixture('category_product_list_url'),
+        lazy_fixture('product_detail_url'),
+    )
+)
+def test_get_routes_availability_for_auth_user(author_client, name):
+    response = author_client.get(name)
+    assert response.status_code == HTTPStatus.OK
